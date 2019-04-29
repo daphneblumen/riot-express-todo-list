d827b476c811a7c7b0ab4791a9ca93c3f3e02cec
Daphne Blumenthal <daphne@tierrait.com>
2019-04-29 02:03:21 -0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.159.130.254',
+      host: '3.88.177.44',
       username: 'ubuntu',
       privateKey: 'bleh.pem'
     })
